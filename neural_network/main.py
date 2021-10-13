874528ea144faa7a95b8822e2e60240c5b897bed
Anita Tereszczuk <anita.tereszczuk@gmail.com>
2021-10-13 13:16:16 +0200

    remove unused lib

--- neural_network/main.py
+++ neural_network/main.py
@@ -1,9 +1,6 @@
-import numpy as np
-import pandas as pd
-
-from network_module import Network
-from layer_module import Layer
-from activation_module import Activation
+#from network_module import Network
+#from layer_module import Layer
+#from activation_module import Activation
 from load_data_module import read_all_letters
 
 if __name__ == "__main__":
