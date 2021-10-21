b485896ed3944568c23ab15990694103ebdd5044
216929 Jakub Wróbel <37510383+jkwrobel@users.noreply.github.com>
2021-10-21 20:29:15 +0200

    Updated SGA

--- sga/main.py
+++ sga/main.py
@@ -1,16 +1,32 @@
 import configparser
+import random
+import numpy as np
 
-from neuron import Neuron
+from sga import SGA
+
+
+
+
 
 if __name__ == '__main__':
-    criterion_method_string = 'GENERAL'
+    config_section = 'GENERAL'
 
     print("Program that simulates a single neuron using the delta rule.")
 
     config = configparser.ConfigParser()
     config.read("config.ini")
-    config = config[criterion_method_string]
-    neuron = Neuron(config)
-    neuron.train(int(config['epoch_count']), float(config['target_precision']))
-    neuron.print_weights()
+    config = config[config_section]
+    target_precision = int(config['target_precision'])
+    range_start = float(config['range_start'])
+    range_end = float(config['range_end'])
+    chromosome_count = int(config['chromosome_count'])
+    x_digits_count = len(bin(int((range_end - range_start) * 10 ** target_precision))) - 2
 
+    run = 0
+    gen = 0
+
+
+    print(x_digits_count)
+
+    sample_x = list(np.random.randint(0, 2, x_digits_count))
+    print(sample_x)
--- sga/sga.py
+++ sga/sga.py
@@ -5,20 +5,54 @@ import random as rand
 class SGA:
     def __init__(self, config):
         self.config = config
-        for i in range(self.weight_count):
-            self.weights.append(rand.uniform(self.weight_init_lower_bound, self.weight_init_upper_bound))
+        self.run = 0
+        self.gen = 0
+        self.target_precision = int(config['target_precision'])
+        self.target_gen_count = int(config['target_gen_count'])
+        self.range_start = float(config['range_start'])
+        self.range_end = float(config['range_end'])
+        self.chromosome_count = int(config['chromosome_count'])
+        self.crossing_prob = float(config['crossing_prob'])
+        self.mutation_prob = float(config['mutation_prob'])
+        self.reproduction_prob = 1 - self.crossing_prob - self.mutation_prob
+        self.x_digits_count = len(bin(int((self.range_end - self.range_start) * 10 ** self.target_precision))) - 2
+        self.this_chromosomes = [None] * self.chromosome_count
+        self.this_chromosomes_fitness = [None] * self.chromosome_count
+        self.next_chromosomes = [None] * self.chromosome_count
+        self.fittest_chromosomes = [None] * self.chromosome_count
 
-    def train(self, epoch_count=1, target_error=0.1):
-        for i in range(epoch_count):
-            if i % 5 == 0:
-                print('Current epoch: ' + str(i) + ' Current error: ' + str(self.calculate_error()))
-            for train_pattern in self.train_patterns:
-                current_output = self.compute_vector(train_pattern[:len(train_pattern) - 1])
-                output_delta = train_pattern[-1] - current_output
-                for j in range(self.weight_count):
-                    self.weights[j] += self.training_coefficient * output_delta * train_pattern[j]
+    def do_single_run(self):
+        self.create_initial_population()
 
-            if self.calculate_error() <= target_error:
-                print('Current epoch: ' + str(i) + ' Current error: ' + str(self.calculate_error()))
-                return
+        while ~self.check_stop_criteria():
+            for i in range(self.chromosome_count):
+                self.this_chromosomes_fitness[i] = fitness_function_bin(self.this_chromosomes[i])
 
+            j = 0
+            while j < self.chromosome_count:  # I f...... HATE python for loops
+                rand_prob = rand.uniform(0, self.reproduction_prob + self.crossing_prob + self.mutation_prob)
+                if rand_prob <= self.reproduction_prob:
+                    print('reproduction')
+                elif rand_prob <= self.reproduction_prob + self.crossing_prob:
+                    print('crossing')
+                    j += 1
+                else:
+                    print('mutation')
+                j += 1;  # And give me my ++ operator back. >:C
+
+    def check_stop_criteria(self):
+        return self.gen == self.target_gen_count
+
+    def create_initial_population(self):
+        for i in range(self.chromosome_count):
+            self.this_chromosomes.append(list(np.random.randint(0, 2, self.x_digits_count)))
+
+
+
+
+def fitness_function_dec(x):
+    return (np.e ** x * np.sin(10 * np.pi * x) + 1) / x + 5
+
+
+def fitness_function_bin(x):
+    return fitness_function_bin(int(x, 2))
